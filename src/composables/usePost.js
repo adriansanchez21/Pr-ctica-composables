a47db0c83d436319dab5309423b9af4daac68f68
nerea <nerea@ubuntu.myguest.virtualbox.org>
2023-12-15 15:21:14 +0100

    medio acabado

--- src/composables/usePost.js
+++ src/composables/usePost.js
@@ -3,19 +3,23 @@ export default function usePost() {
 
     const posts = ref([])
     const post = ref(null)
-    const baseURL = "https://jsonplaceholder.typicode.com/posts"
-    const fetchAll = async () => {
-        const response = await fetch(baseURL)
-        posts.value = await response.json()
-    }
-    
+    const baseURL = "https://jsonplaceholder.typicode.com/posts/"
 
     const llegirPosts = async () => {
         const response = await fetch(baseURL)
         posts.value = await response.json()
     }
-return {
-       	posts,
-       	llegirPosts,
-   	}
+
+    const llegirPost = async (id) => {
+        const urlPost = baseURL + id
+        const response = await fetch(urlPost)
+        post.value = await response.json()
+    }
+
+    return {
+        posts,
+        llegirPosts,
+        post,
+        llegirPost,
+    }
 }
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if="post && user">
+  <!-- <div> -->
     <h1 class="text-3xl">{{ post.title }}</h1>
     <div class="text-gray-500 mb-10">by {{ user.name }}</div>
     <div>{{ post.body }}</div>
@@ -7,8 +8,19 @@
 </template>
 <script setup>
 import { useRoute } from 'vue-router';
-import llegirPost from '../composables/llegirPost';
+import usePost from '../composables/usePost.js';
+import useUser from '../composables/useUser'
 const route = useRoute()
 
-const {post} = llegirPost(route.params.id)
+const {post, llegirPost} = usePost()
+const id = route.params.id
+const {user, llegirUser} = useUser()
+
+const info = async () => {
+  await llegirPost(id)
+  llegirUser(post.value.userId)
+}
+
+info()
+
 </script>
